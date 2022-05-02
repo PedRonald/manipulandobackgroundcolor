d3e5bba3e149098c07274750f63f351601d79851
PedRonald <pronalmsvg@gmail.com>
2022-05-02 12:34:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function verde(){
+    document.getElementById("demo").style.backgroundColor = "green";
+}
+function vermelho(){
+    document.getElementById("demo").style.backgroundColor = "red";
+}
+function azul(){
+    document.getElementById("demo").style.backgroundColor = "blue";
+}
